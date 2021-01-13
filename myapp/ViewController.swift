8eff1983352c2ca1af4547cc8873deb5c15806ad
aliciacastro <aliciam.castroh@gmail.com>
2021-01-12 23:22:58 -0600

    Create base proj.

---
+++ myapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by Alicia Monserrath Castro Hernandez on 12/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
